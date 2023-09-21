4512c032e99473615d8cf0ba3f7d2c9db4e6e62a
yaj55billy <yaj55ushydna@gmail.com>
2023-09-21 09:45:50 +0800

    init commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      "viewport": "width=device-width, initial-scale=1, minimum-scale=1, user-scalable=0",
+      "title": "Billy's Portfolio",
+      "meta": [
+        { charset: 'utf-8' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'description', content: '' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: "Billy's Portfolio" },
+        { property: 'og:title', content: "Billy's Portfolio" },
+        { property: 'og:description', content: '' },
+        { property: 'og:image', content: '' },
+        { property: 'og:url', content: '' }
+      ],
+
+      link: [
+        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css' }
+      ],
+      script: [
+
+      ]
+    }
+  },
+
+  devtools: { enabled: true },
+  css: [
+    '@/assets/styles/main.sass',
+  ],
+})
