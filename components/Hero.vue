5fda73b5bf2e0960f62f649bcb9673cf3e794d31
yaj55billy <yaj55ushydna@gmail.com>
2023-09-25 20:12:08 +0800

    section hero(25%)

--- components/Hero.vue
+++ components/Hero.vue
@@ -14,15 +14,18 @@ defineExpose({
 		<div class="container">
 			<div class="hero__wrapper">
 				<div class="hero__content">
-					<h1 class="hero__title">Hi, I am Billy,<br />Web Developer.</h1>
+					<h1 class="hero__title">Hi, I am Billy,<br />Front-End Developer.</h1>
 					<p class="hero__description">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-						voluptates ad numquam, officiis accusamus similique voluptate! Sint
-						corrupti soluta, expedita dolores.
+						哈囉，我是 Billy，一名持續精進的前端工程師。在前端領域已有 4
+						年的開發經驗，並且熟悉如何與團隊配合、溝通，以製作出網站。
 					</p>
 				</div>
 				<div class="hero__avatar">
-					<!-- <img src="assets/img/hero.jpg" alt="Billy Ji" class="hero__img" /> -->
+					<img
+						src="https://i.imgur.com/6uC6FLI.png"
+						alt="Billy Ji"
+						class="hero__img"
+					/>
 				</div>
 			</div>
 		</div>
