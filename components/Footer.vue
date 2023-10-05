542c0f0a11795d40081bf82fc6ed7a11baab36bd
yaj55billy <yaj55ushydna@gmail.com>
2023-10-05 10:45:55 +0800

    footer(100%)

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,23 @@
 <script setup></script>
 <template>
-	<footer>Footer</footer>
+	<footer id="footer" class="footer">
+		<div class="container">
+			<div class="footer__content">
+				<div class="hero__iconbox">
+					<a
+						href="https://github.com/yaj55billy"
+						target="_blank"
+						class="hero__icon"
+						><Icon name="bxl:github"
+					/></a>
+					<a href="mailto:yaj55ushydna@gmail.com" class="hero__icon"
+						><Icon name="bxl:gmail"
+					/></a>
+				</div>
+				<p class="footer__text">© Copyright 2023 Billy Ji</p>
+			</div>
+		</div>
+	</footer>
 </template>
 
 <style></style>
