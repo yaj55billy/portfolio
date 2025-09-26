3ef81cbce0428569d7157d7e68b34fe49a63abb3
yaj55billy <yaj55ushydna@gmail.com>
2025-09-26 08:50:27 +0800

    adjust experience info

--- components/Experience.vue
+++ components/Experience.vue
@@ -1,27 +1,27 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted } from 'vue';
 const { $gsap } = useNuxtApp();
 const experiences = ref([
 	{
-		company: "六角學院",
-		years: "2023 — 2024",
-		position: "前端工程助教",
+		company: '六角學院',
+		years: '2023 — 2025',
+		position: '前端工程助教',
 		content:
-			"在 Vue 作品實戰直播班、Node.js 前後端入門班擔任助教。回覆同學在課程遇到的相關問題，像是 Vue 跟 Node 的觀念，或者引導同學看到（解決）實作上發生的 Bug，以及相關的前端疑問（例如：業界的狀況）。",
+			'在切版直播班、Vue 直播班、React 直播班、Node.js 前後端入門班擔任助教。協助學員解決課程、作業中遇到的問題，並提供相關技術指引與建議，以及回覆前端相關的疑問（例如：業界的狀況）。',
 	},
 	{
-		company: "芯禾數位行銷",
-		years: "2018 — 2021",
-		position: "前端工程師",
+		company: '芯禾數位行銷',
+		years: '2018 — 2021',
+		position: '前端工程師',
 		content:
-			"公司專案類型主要為銀行業的活動型網站建置，我主要負責的部分是以 HTML、SASS 做響應式切版，使用 jQuery 、JavaScript 做網頁互動、串接第三方 API (Google Maps)，並與企劃、設計師、後端工程師合作完成專案。在任職期間跟前端夥伴導入 GIT 版本控制，開始制定相關文件，解決協作、檔案管理的問題。",
+			'公司專案類型主要為銀行業的活動型網站建置，我主要負責的部分是以 HTML、SASS 做響應式切版，使用 jQuery 、JavaScript 做網頁互動、串接第三方 API (Google Maps)，並與企劃、設計師、後端工程師合作完成專案。在任職期間跟前端夥伴導入 GIT 版本控制，開始制定相關文件，解決協作、檔案管理的問題。',
 	},
 	{
-		company: "森德網站設計",
-		years: "2017 — 2018",
-		position: "前端工程師",
+		company: '森德網站設計',
+		years: '2017 — 2018',
+		position: '前端工程師',
 		content:
-			"公司專案類型主要為企業形象網站建置，我主要負責的部分是以 HTML、CSS 做響應式切版，使用 jQuery 做網頁互動效果，並與企劃、設計師、後端工程師合作完成專案。在專案中適時導入 Bootstrap 提升開發效率，任職期間也有經手過中大型網站的經驗。",
+			'公司專案類型主要為企業形象網站建置，我主要負責的部分是以 HTML、CSS 做響應式切版，使用 jQuery 做網頁互動效果，並與企劃、設計師、後端工程師合作完成專案。在專案中適時導入 Bootstrap 提升開發效率，任職期間也有經手過中大型網站的經驗。',
 	},
 ]);
 const experienceRefs = ref([]);
@@ -33,9 +33,9 @@ onMounted(() => {
 			$gsap.from(section, {
 				scrollTrigger: {
 					trigger: section,
-					start: "top 80%",
-					end: "bottom 20%",
-					toggleClass: "active",
+					start: 'top 80%',
+					end: 'bottom 20%',
+					toggleClass: 'active',
 					// markers: true,
 					scrub: true, // 使動畫與滾動同步
 				},
